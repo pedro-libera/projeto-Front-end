d376c5534dff8c8b4b0796831a17a896c20a62c4
Diogo Agra Alexandria <diogo.alexandria@m4u.com.br>
2020-08-08 19:35:21 -0300

    Adiçao do component footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from '../../assets/images/logo.png';
+import instagram from '../../assets/images/instagram.png';
+import facebook from '../../assets/images/facebook.png';
+import './styles.css';
+
+function Footer() {
+    return (
+        <React.Fragment>
+            <footer>
+                <img src={logo} alt="Logo projeto pet"/>
+                <div className="social-media">
+                    <p>Nos sigam:</p>
+                    <div className="instagram">                        
+                        <img src={instagram} alt="instagram logo"></img>
+                        <p>@projetopet</p>
+                    </div>
+                    <div className="facebook">
+                        <img src={facebook} alt="facebook logo"></img>
+                        <p>/projetopet</p>
+                    </div>
+                </div>
+
+            </footer>
+        </React.Fragment>
+    )
+}
+
+export default Footer;
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import './styles.css';
 
 function Landing() {
     return (
         <React.Fragment>
-            <h1>Landing</h1>
+            <div id="landing-body">
+                <h1>Landing</h1>
+            </div>            
+            <Footer/>
         </React.Fragment>
     )
 }
