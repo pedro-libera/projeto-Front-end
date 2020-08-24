2149d22cbe540059db07d15a6d18f5cd737fd49b
Diogo Agra Alexandria <diogo.alexandria@m4u.com.br>
2020-08-23 22:43:47 -0300

    Criaçao de pagina de registro

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,12 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
 
 function Register() {
     return (
         <React.Fragment>
             <div id="register-page">
                 <div className="form-box">
-                    <h1>Register</h1>
-
+                    <div className="title">
+                        <h1>Register</h1>
+                    </div>
+                    <form action="">
+                        <div className="input-block">
+                            <label htmlFor="">
+                                Nome completo
+                            </label>
+                            <input type="text" />
+                        </div>
+                        <div className="input-block">
+                            <label htmlFor="">
+                                E-mail
+                            </label>
+                            <input type="text" />
+                        </div>
+                        <div className="input-block">
+                            <label htmlFor="">
+                                Senha
+                            </label>
+                            <input type="text" />
+                        </div>
+                        <div className="input-block">
+                            <label htmlFor="">
+                                Confirme a senha
+                            </label>
+                            <input type="text" />
+                        </div>
+                        <div className="checkbox-block">
+                            <input type="checkbox" />
+                            <label htmlFor="">
+                                Eu aceito os <a href="#">termos de uso</a> e <a href="#">políticas de privacidade</a>
+                            </label>
+                        </div>
+                        <div className="button-block">
+                            <button type="submit">Cadastrar</button>
+                        </div>
+                    </form>
+                    <div className="login-redirect">
+                        <p>Ja é cadastrado?</p>
+                        <Link to="/login">Faça seu login aqui</Link>
+                    </div>
                 </div>
             </div>
         </React.Fragment>
